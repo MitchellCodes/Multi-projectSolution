90bcc436a4b4ab17b18204febc76960f5db67bcd
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2021-11-09 13:07:20 -0800

    Added combo box and button to form

--- ChuckNorrisWinForm/ChuckNorrisForm.Designer.cs
+++ ChuckNorrisWinForm/ChuckNorrisForm.Designer.cs
@@ -28,12 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cbxCategories = new System.Windows.Forms.ComboBox();
+            this.btnGetRandomJoke = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbxCategories
+            // 
+            this.cbxCategories.FormattingEnabled = true;
+            this.cbxCategories.Location = new System.Drawing.Point(300, 96);
+            this.cbxCategories.Name = "cbxCategories";
+            this.cbxCategories.Size = new System.Drawing.Size(121, 23);
+            this.cbxCategories.TabIndex = 0;
+            // 
+            // btnGetRandomJoke
+            // 
+            this.btnGetRandomJoke.Location = new System.Drawing.Point(206, 284);
+            this.btnGetRandomJoke.Name = "btnGetRandomJoke";
+            this.btnGetRandomJoke.Size = new System.Drawing.Size(374, 73);
+            this.btnGetRandomJoke.TabIndex = 1;
+            this.btnGetRandomJoke.Text = "Get Random Joke";
+            this.btnGetRandomJoke.UseVisualStyleBackColor = true;
+            // 
+            // ChuckNorrisForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnGetRandomJoke);
+            this.Controls.Add(this.cbxCategories);
+            this.Name = "ChuckNorrisForm";
+            this.Text = "Chuck Norris Jokes";
+            this.Load += new System.EventHandler(this.ChuckNorrisForm_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private ComboBox cbxCategories;
+        private Button btnGetRandomJoke;
     }
 }
--- ChuckNorrisWinForm/ChuckNorrisForm.cs
+++ ChuckNorrisWinForm/ChuckNorrisForm.cs
@@ -6,5 +6,10 @@ namespace ChuckNorrisWinForm
         {
             InitializeComponent();
         }
+
+        private void ChuckNorrisForm_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
